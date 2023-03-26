52ced84c3997fab09c4555702b78b52253252d40
Karenyulier <karenyulierr@gmail.com>
2023-03-25 21:20:40 -0500

    [add] cerrar sesion y ajustes del muro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,10 +16,29 @@
             <h1 class="text-3xl font-black">
                 DevStagram
             </h1>
-            <nav class="flex gap-2 items-center">
-                <a class="font-bold uppercase text-gray-600 text-sm " href="#">Login</a>
-                <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('crear-cuenta') }}">Crear Cuenta</a>
-            </nav>
+            @auth
+                <nav class="flex gap-2 items-center">
+                    <a class="font-bold text-gray-600 text-sm " href="#">
+                        Hola:
+                        <span class="font-normal">
+                            {{ auth()->user()->username }}
+                        </span>
+                    </a>
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button type="submit" class="font-bold uppercase text-gray-600 text-sm ">Cerrar sesión</button>
+                    </form>
+                </nav>
+            @endauth
+
+            @guest
+                <nav class="flex gap-2 items-center">
+                    <a class="font-bold uppercase text-gray-600 text-sm " href="{{ route('login') }}">Login</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('crear-cuenta') }}">Crear Cuenta</a>
+                </nav>
+            @endguest
+
+
         </div>
     </header>
 
