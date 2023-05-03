6c3108665a04af45b85ab1c9141408fd71bd3150
KarenYulier <krodriguez@arcetec.net>
2023-05-02 21:12:56 -0500

    [Update] Imagenes

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,12 +5,17 @@
 @endsection
 
 
+@push('style')
+    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+@endpush
 
 @section('contenido')
     <div class="md:flex md:items-center">
         <div class="md:w-1/2 px-10">
-            <form action="/imagenes" id="dropzone"
-                class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center">
+            <form action="{{ route('imagenes.store') }}" method="POST" id="dropzone"
+                class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center"
+                enctype="multipart/form-data">
+                @csrf
             </form>
         </div>
         <div class="md:w-1/2 p-10 bg-white rounded-lg shadow-xl mt-10 md:mt-0">
